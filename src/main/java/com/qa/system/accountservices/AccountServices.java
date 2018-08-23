657017b918a8d19abbe3e67a65373a9e321e76c0
a1bertt <a1bert@hotmail.co.uk>
2018-08-23 16:07:21 +0100

    commit 3

--- src/main/java/com/qa/system/accountservices/AccountServices.java
+++ src/main/java/com/qa/system/accountservices/AccountServices.java
@@ -3,17 +3,21 @@ package com.qa.system.accountservices;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.json.JSONObject;
+
 import com.qa.system.accounts.Accounts;
 
 public class AccountServices {
 
 	private Map<Integer, Accounts> accountMap;
+	JSONObject gson;
 	
 	private Integer counter = 1;
 	
 	public AccountServices()
 	{
 		accountMap = new HashMap<>();
+		gson = new JSONObject(accountMap);
 	}
 	
 	public void addAccountToMap(Accounts accountToAdd)
